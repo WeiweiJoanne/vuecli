03cad2dc13d97b8b071d46412f239ff55089fcdd
joannewei <joannewei0921@gmail.com>
2020-11-12 02:22:14 +0800

    api error

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</div>
 </template>
 
 <script>
@@ -12,6 +12,12 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  created () {
+    const api = 'http://localhost:8080/api/joanne/products'
+    this.$http.get(api).then((response) => {
+      console.log(response.data)
+    })
   }
 }
 </script>
